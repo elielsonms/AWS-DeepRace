8e518b7187b8214ebf77e62b708fd35c540c0a78
Elielson <elielsonms@gmail.com>
2019-06-01 00:15:03 -0300

    Added some bias to ensure is near of the ideal value
    Giving good reward when car angle and car steering angle added is on a good bias with the track angle

--- reward_function.py
+++ reward_function.py
@@ -1,30 +1,39 @@
 MAX_SPEED = 5
 SPEED_GRANULARITY = 3
+BIAS_ANGLE_DIFF = 5
+DEMOTIVATE_VALUE = 1e-3
 
 def is_on_straight_line(params):
-    #If on distance between two waypoints could exist an curse this won`t work
-    return track_angle(params) == params['heading']
+    #If on distance between two waypoints could exist an curve this won`t work
+    return fit_bias(fullAngle(track_angle(params)),
+                    fullAngle(params['heading']), BIAS_ANGLE_DIFF)
+
+def is_curve_ahead(params):
+    return fit_bias(fullAngle(track_angle(params)),
+                        fullAngle(track_angle_ahead(params)), 1)
 
 # If is on straight line, keep increasing the speed
+# If is there curve ahead, reduce speed
 # Bad reward if is on the lowest granularity
 def reward_for_straight_line(params):
+    curve_ahead = is_curve_ahead(params)
     if params['speed'] == MAX_SPEED :
-        return max_reward(params)
+        return lowest_reward(params) if curve_ahead  else  max_reward(params)
     elif params['speed'] > MAX_SPEED/SPEED_GRANULARITY:
-        return 0.5
+        return parametized_reward(0.5,params)
     else:
-        return lowest_reward(params)
-    
+        return max_reward(params) if curve_ahead else lowest_reward(params)
 
 # If the track angle is on differente signal than the car angle, bad reward
 def reward_for_curve(params):
-    car = params['steering_angle']
-    track = angleOn180(track_angle(params))
+    sterring = params['steering_angle']
+    car = fullAngle(params['heading'])
+    track = fullAngle(track_angle(params))
 
-    if car*track < 0 :
-        return lowest_reward(params)
+    if(fit_bias(track, car+sterring,8)):
+        return max_reward(params)
     else:
-        return max_reward(params) 
+        return lowest_reward(params)
 
 def reward_function(params):
     reward = 1
@@ -47,6 +56,11 @@ def angleOn180(angle):
         return angle - 360
     return angle
 
+def fullAngle(angle):
+    if(angle < 0):
+        return 360 + angle
+    return angle
+
 def track_angle(params):
     closest_waypoints = params['closest_waypoints']
     waypoints =  params['waypoints']
@@ -55,13 +69,27 @@ def track_angle(params):
 
     return angle(next_point,prev_point)
 
+def track_angle_ahead(params):
+    waypoints_ahead = params['closest_waypoints'][1]
+    waypoints =  params['waypoints']
+    prev_point = waypoints[waypoints_ahead]
+    next_point = waypoints[min(waypoints_ahead+1,49)]#avoid arrayIndexOutOfBounds
+
+    return angle(next_point,prev_point)
+
 #give higher reward if is in a more advanced progress
-#Evaluate if really worth
+#TODO: Evaluate if really worth
 def max_reward(params):
     return 1*params['progress']
 
+def parametized_reward(reward,params):
+    return reward*params['progress']
+
 def lowest_reward(params):
-    return 1e-3
+    return DEMOTIVATE_VALUE
+
+def fit_bias(angle1, angle2, bias):
+    return abs(angle1-angle2) <= bias
 
 aws_params = {
     "all_wheels_on_track": True,    # flag to indicate if the vehicle is on the track
@@ -81,10 +109,13 @@ aws_params = {
 
 
 
-print(angle([1,1],[3,3])) #45
-print(angle([1,1],[0,0])) #- 135 / 225
-print(angle([1,1],[0,1])) #180 / -180
-print(angle([0,1], [1,1])) #0/360
-print(angle([0,1], [2,2])) #0/360
+print(fullAngle(angle([1,1],[3,3]))) #45
+print(fullAngle(angle([1,1],[0,0]))) #- 135 / 225
+print(fullAngle(angle([1,1],[0,1]))) #180 / -180
+print(fullAngle(angle([0,1], [1,1]))) #0/360
+print(fullAngle(angle([0,1], [2,2]))) #0/360
+
+print(fullAngle(angle([0,1], [0,1]))) #0/360
+
 
 #reward_function(aws_params)
